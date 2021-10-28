9585ceaaf4ffb466adfe05af836124dc0ac470e7
Erik Sundell <erik.i.sundell@gmail.com>
2021-10-28 13:33:16 +0200

    release 1.0.0

--- firstuseauthenticator/_version.py
+++ firstuseauthenticator/_version.py
@@ -4,10 +4,10 @@
 # Distributed under the terms of the Modified BSD License.
 
 version_info = (
+    1,
     0,
-    14,
-    2,
-    'dev',  # comment-out this line for a release
+    0,
+    # 'dev',  # comment-out this line for a release
 )
 __version__ = '.'.join(map(str, version_info[:3]))
 
